42e4820548b84345789a1ae79bbcf04acbdc3dbc
Viktoria <zaharova181114@gmail.com>
2021-06-03 09:08:11 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,21 @@
+// accordeon
+function accordeon() {
+    var panel = $('.panel_heading');
+
+    if (panel.hasClass('in')) {
+        $('.in').find('.block_hover').slideDown();
+    }
+
+    $('.panel_heading .block_title').on('click', function () {
+        $(this).parent().toggleClass('in').find('.block_hover').slideToggle();
+    });
+}
+
+accordeon();
+
+$('.reviews-slider').slick({
+    slidesToShow: 1,
+    appendArrows: '.slider-nav',
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.png" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-left.png" alt=""></button>',
+});
